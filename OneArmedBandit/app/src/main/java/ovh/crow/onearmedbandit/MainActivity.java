4c0422e2ff7aa698babd2b7cb125282c14ab2e6a
Crowdedlight <crowdedhd@gmail.com>
2017-02-25 01:05:29 +0100

    added timeout. seems like the timeout thread doesnt get killed correctly when user stop the app. Carries over to next game

--- OneArmedBandit/app/src/main/java/ovh/crow/onearmedbandit/MainActivity.java
+++ OneArmedBandit/app/src/main/java/ovh/crow/onearmedbandit/MainActivity.java
@@ -9,14 +9,22 @@ import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.Shape;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView currBet;
 
     private int betAmount = 0;
+
+    //thread declared to be able to stop it again
+    Future backgroundTimeOut;
 
     //Get start button
     private Button btnStart, btnStop1, btnStop2, btnStop3;
@@ -99,6 +110,9 @@ public class MainActivity extends AppCompatActivity {
         if (!fruitAnimation1.isRunning() && !fruitAnimation2.isRunning() && !fruitAnimation3.isRunning()) {
             //call method to check winnings
             CheckWin();
+
+            //stop backgroud thread
+            backgroundTimeOut.cancel(true);
         }
     }
 
@@ -158,6 +172,7 @@ public class MainActivity extends AppCompatActivity {
         int duration = Toast.LENGTH_LONG;
 
         Toast toast = Toast.makeText(context, msg, duration);
+        //toast.setGravity(Gravity.TOP|Gravity.START, 10,30);
         toast.show();
 
         //reset bet amount
@@ -186,6 +201,13 @@ public class MainActivity extends AppCompatActivity {
         fruitAnimation1.start();
         fruitAnimation2.start();
         fruitAnimation3.start();
+
+        //start background thread for timeout if user doesn't click
+        //backgroundTimeout = new Thread(new MyRunnable());
+        //backgroundTimeout.start();
+
+        ExecutorService threadPoolExecutor = Executors.newSingleThreadExecutor();
+        backgroundTimeOut = threadPoolExecutor.submit(new MyRunnable());
     }
 
     public void ShowBetting(View view) {
@@ -214,6 +236,73 @@ public class MainActivity extends AppCompatActivity {
                     Log.v("app", "RESULT_CANCELED from RecieveMessageActivity");
                     break;
             }
+        }
+    }
+
+    private class MyHandler extends Handler {
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch(msg.what) {
+                case 1:
+                    if (fruitAnimation1.isRunning())
+                        btnStop1.performClick();
+                    break;
+                case 2:
+                    if (fruitAnimation2.isRunning())
+                        btnStop2.performClick();
+                    break;
+                case 3:
+                    if (fruitAnimation3.isRunning())
+                        btnStop3.performClick();
+                    break;
+            }
+        }
+    }
+
+    //handler
+    final MyHandler mHandler = new MyHandler();
+
+    private class MyRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            int max = 12000;
+            int min = 8000;
+            Random rnd = new Random();
+
+            int time = rnd.nextInt(max - min + 1) + min;
+
+            //Sleep first time
+            try {
+                Thread.sleep(time);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            //stop spinner 1
+            mHandler.sendEmptyMessage(1);
+
+            //sleep second time, 2.5 sec
+            try {
+                Thread.sleep(2500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            //stop spinner 2
+            mHandler.sendEmptyMessage(2);
+
+
+            //sleep second time, 2.5 sec
+            try {
+                Thread.sleep(2500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            //stop spinner 3
+            mHandler.sendEmptyMessage(3);
         }
     }
 }
